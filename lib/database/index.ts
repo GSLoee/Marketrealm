2fd0efe97468dc30c25f0d2b952ca4a084f8c515
GSLoee <gurnoorloee001@gmail.com>
2024-07-04 20:55:00 -0400

    marketrealm

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,18 +1,20 @@
-import { CircleDashed } from 'lucide-react'
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn:null, promise: null}
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn; 
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'Marketrealm',
-        bufferCommands: true,
-    })
-    cached.conn = await cached.promise;
+  if (cached.conn) return cached.conn;
 
-    return cached.conn; 
-} 
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'Marketrealm',
+    bufferCommands: false,
+  })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
