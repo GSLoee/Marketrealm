5f0f55ea889376d340dd749834efc84410f7cc2f
GSLoee <gurnoorloee001@gmail.com>
2024-07-09 18:17:00 -0400

    address update

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,60 +1,48 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { buffer } from 'micro';
-import Stripe from 'stripe';
-import { createOrder } from '@/lib/actions/order.actions';
+import stripe from 'stripe'
+import { NextResponse } from 'next/server'
+import { createOrder } from '@/lib/actions/order.actions'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
-});
 
-// New route segment configuration
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const buf = await buffer(req);
-    const sig = req.headers['stripe-signature']!;
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-
-    let event: Stripe.Event;
-
+export async function POST(request: Request) {
+    const body = await request.text()
+  
+    const sig = request.headers.get('stripe-signature') as string
+    const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+  
+    let event
+  
     try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+      event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
     } catch (err) {
-      console.error(`Webhook signature verification failed.`, err);
-      return res.status(400).send(`Webhook signature verification failed.`);
+      return NextResponse.json({ message: 'Webhook error', error: err })
     }
-
-    if (event.type === 'checkout.session.completed') {
-      const session = event.data.object as Stripe.Checkout.Session;
-      const { metadata, shipping_details, amount_total, id } = session;
-      
-      const orderData = {
-        stripeId: id,
-        productId: metadata?.productId,
-        buyerId: metadata?.buyerId,
-        totalAmount: amount_total ? (amount_total / 100).toString() : '0',
-        address: {
-          city: shipping_details?.address?.city || '',
-          country: shipping_details?.address?.country || '',
-          line1: shipping_details?.address?.line1 || '',
-          line2: shipping_details?.address?.line2 || '',
-          postal_code: shipping_details?.address?.postal_code || '',
-        }
+  
+    // Get the ID and type
+    const eventType = event.type
+  
+    // CREATE
+    if (eventType === 'checkout.session.completed') {
+        const session = event.data.object as stripe.Checkout.Session;
+        const { metadata, shipping_details, amount_total, id } = session;
+        
+        const orderData = {
+          stripeId: id,
+          productId: metadata?.productId,
+          buyerId: metadata?.buyerId,
+          totalAmount: amount_total ? (amount_total / 100).toString() : '0',
+          address: {
+            city: shipping_details?.address?.city || '',
+            country: shipping_details?.address?.country || '',
+            line1: shipping_details?.address?.line1 || '',
+            line2: shipping_details?.address?.line2 || '',
+            postal_code: shipping_details?.address?.postal_code || '',
+          }
       };
-
-      await createOrder(orderData);
+      console.log('_______________', orderData)
+  
+      const newOrder = await createOrder(orderData)
+      return NextResponse.json({ message: 'OK', order: newOrder })
     }
-
-    res.json({ received: true });
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+  
+    return new Response('', { status: 200 })
   }
-};
-
-export default webhookHandler;
